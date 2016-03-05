5101acd41cb7fadb418e5051882a722d579aba07
Luis Canales <lacanales@ucdavis.edu>
2016-03-04 18:44:05 -0800

    Added stem for JSON updating in the future,  removed unneeded file.

--- main.go
+++ main.go
@@ -3,8 +3,21 @@ package main
 import (
 	_ "decision2016/routers"
 	"github.com/astaxie/beego"
+	"net/http"
+	"encoding/json"
 )
 
+func checkJSON(url string,target interface{}) error {
+	r, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
 func main() {
+  //go checkJSON(jsonURL,json)
 	beego.Run()
 }
